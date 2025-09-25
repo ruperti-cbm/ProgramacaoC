cec31830e78c3b5b71f3d62246850b4cf3181d40
ruperti-cbm <202502914075@alunos.estacio.br>
2025-09-25 20:10:46 -0300

    Create math.c

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+   int numero1, numero2;
+   int soma, subtracao, multiplicacao, divisao;
+
+   soma = numero1 + numero2;
+
+   subtracao = numero1 - numero2;
+
+   multiplicacao = numero1 * numero2;
+
+   divisao = numero1/numero2
+}
